ef27de619b047b7cb270430a0d786a9c3fff3737
dataconsumer101 <60371898+dataconsumer101@users.noreply.github.com>
2021-03-17 21:24:22 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-server = app.server
+# server = app.server
 
 # loop until all records are pulled
 def get_query(flavor,tree,borough):
