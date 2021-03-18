4db1d1dfbf1b8bf540161b4ef54586511a05e4a5
dataconsumer101 <60371898+dataconsumer101@users.noreply.github.com>
2021-03-17 21:55:47 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -10,8 +10,10 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-# server = app.server
+server = app.server
 
+
+# get dataframe for outputs
 # loop until all records are pulled
 def get_query(flavor,tree,borough):
 
@@ -46,6 +48,7 @@ def get_query(flavor,tree,borough):
 
         i += 1    
 
+    # calculate share % for each section
     if flavor == 'datatable':
         df_treesum = df.groupby(['spc_common', 'boroname']).sum().reset_index()
         df_treesum.columns = ['spc_common', 'boroname', 'total']
@@ -72,9 +75,6 @@ def get_query(flavor,tree,borough):
     return df
 
 
-
-
-
 # For Species Drop Down
 # get distinct list of tree species
 query = ('https://data.cityofnewyork.us/resource/nwxe-4ae8.json?' +\
@@ -98,43 +98,9 @@ borough = pd.read_json(query)
 borough = borough.dropna()
 
 
-# # for Data Table
-# # get health
-# query = ('https://data.cityofnewyork.us/resource/nwxe-4ae8.json?' +\
-#     '$select=spc_common,health,boroname,count(tree_id)' +\
-#         '&$group=spc_common,health,boroname')
-
-# df = get_query(query)
-
-# df_treesum = df.groupby(['spc_common', 'boroname']).sum().reset_index()
-# df_treesum.columns = ['spc_common', 'boroname', 'total']
-
-# df_health = df.merge(df_treesum, on=['spc_common','boroname'])
-# df_health['Share'] = round((df_health.count_tree_id / df_health.total) * 100, 1)
-# df_health['Share'] = df_health['Share'].astype('str') + ' %'
-
-# df_health = df_health[['spc_common', 'boroname', 'health', 'count_tree_id', 'Share']]
-# df_health.columns = ['spc_common', 'boroname', 'Health Condition', 'Count', 'Share']
-# df = []
-
-
-# # For Bar Graph
-# # get steward by health
-# query = ('https://data.cityofnewyork.us/resource/nwxe-4ae8.json?' +\
-#     '$select=spc_common,health,steward,boroname,count(tree_id)' +\
-#         '&$group=spc_common,health,steward,boroname')
-
-# df = get_query(query)
-
-# df_stewsum = df.groupby(['spc_common', 'boroname', 'steward']).sum().reset_index()
-# df_stewsum.columns = ['spc_common', 'boroname', 'steward', 'total']
-
-# df_steward = df.merge(df_stewsum, on=['spc_common','boroname','steward'])
-# df_steward['share'] = round(df_steward.count_tree_id / df_steward.total, 4)
-# df = []
-
+# initial data table place holder
 df_ph = pd.DataFrame(data={'Health Condition': [''], 'Count': 0, 'Share': [0]})
-# fig = px.bar(x = ['steward'], y = [0])
+
 
 app.layout = html.Div(children=[
     html.H3('Data608 Assignment 4 -- New York City Trees'),    
@@ -168,15 +134,14 @@ app.layout = html.Div(children=[
     Input('location', 'value'))
 def update_figure(tree, loc):
 
-    # # data table
-    # df_dt = df_health[(df_health.spc_common == tree) & (df_health.boroname == loc)]
-    # df_dt = df_dt[['Health Condition', 'Count', 'Share']]
+    # data table
     df_dt = get_query('datatable', tree, loc)
 
     # bar graph
-    # df_bg = df_steward[(df_steward.spc_common == tree) & (df_steward.boroname == loc)]
     df_bg = get_query('barchart', tree, loc)    
+
     fig = px.bar(df_bg, x = 'steward', y = 'share', color = 'health', title = 'Tree Health Percent Share by Steward Group')    
+    
     fig.update_layout(
         xaxis_title = 'Steward Group',
         yaxis_title = 'Percent of Trees',
